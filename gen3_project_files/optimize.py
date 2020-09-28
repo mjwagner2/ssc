a1cb431411631e1357ff8b3df71804dab4a4ed7d
Mike Wagner <mjwagner2@gmail.com>
2020-09-28 15:10:20 -0500

    better log messages for the window, consistent output for multi-thread

--- gen3_project_files/optimize.py
+++ gen3_project_files/optimize.py
@@ -7,10 +7,23 @@ import random
 import time
 import os
 
+class global_handler:
+    def __init__(self, sf_interp_provider):
+        self.sf_interp_provider = sf_interp_provider
+        self.total_log_count = 0
 
-#-------------------------------
-def log_entry(x, z, iternum, label):
-    return "{:20s}Iter: {:04d}".format(label, iternum) + ("{:>10s}"*(len(x)+1)).format(*["{:.3f}".format(v) for v in [z]+x])
+    #-------------------------------
+    def log_header(self):
+        labels = "LCOE qsf H_rmin P_ref solarm H_tower dni_des H_rec D_rec_tb D_riser D_dcomr tshours dT_chrg dT_htHX dT_ltHX".split()
+        return "{:39s}".format("") + ("{:>10s}"*(len(labels))).format(*labels)
+
+    def log_entry(self, x, z, q_sf, l_min, iternum, label):
+        if self.total_log_count % 10 == 0:
+            print(self.log_header())
+
+        self.total_log_count += 1
+
+        return "{:20s}Iter: {:04d}".format(label, iternum) + ("{:>10s}"*(len(x)+3)).format(*["{:.3f}".format(v) for v in [z, q_sf, l_min]+x])
 
 #-------------------------------
 # Constraint function evaluation
@@ -44,6 +57,7 @@ def fconst_eval(x, data):
     L_min = G3rec.ReceiverMinimumTubeLength(q_sf_des * 1.e3 / 3)
 
     #The inequality constraints are feasible if positive
+    print("constraint eval: {:f}\t{:f}".format(data.variables.receiver_height, L_min))
     return data.variables.receiver_height - L_min
 
 
@@ -81,47 +95,51 @@ def f_eval(x, data):
 
     if not simok:
         data.current_iteration += 1
-        print("function evaluation NAN", x_unscaled)
+        # print("function evaluation NAN", x_unscaled)
         return float('nan')
         
     lcoe = data.get_result_value('LCOE (real)')
     if lcoe < 0.1 or lcoe > 50:
         lcoe = float('nan')
 
+    #calculate minimum receiver length for this design
+    L_min = G3rec.ReceiverMinimumTubeLength(data.variables.q_sf_des * 1.e3 / 3)
+
     # Track parameters of current best LCOE
-    if lcoe < data.z_best['z']:
+    if lcoe < data.z_best['z'] and data.variables.receiver_height >= L_min:
         data.z_best['z'] = lcoe
         data.z_best['xk'] = [v for v in x_unscaled]
         data.z_best['iter'] = data.current_iteration
+        data.z_best['q_sf_des'] = data.variables.q_sf_des/3
+        data.z_best['l_min'] = L_min
     
 
-    logline = log_entry(x_unscaled, lcoe, data.current_iteration, data.casename)
+    logline = data.global_handler.log_entry(x_unscaled, lcoe, data.variables.q_sf_des/3, L_min, data.current_iteration, data.casename)
     data.optimization_log += "\n" + logline
 
     time_elapsed = time.time() - data.clock_time_start
     timestamp = "{:03d}:{:02d}   ".format( int(time_elapsed/60), int(time_elapsed % 60) )
-
-    L_min = G3rec.ReceiverMinimumTubeLength(data.variables.q_sf_des * 1.e3 / 3)
-    q_sf_des_single_and_lmin = " {q_sf_des_single:.1f} -> {L_min:.2f}   ".format(q_sf_des_single=data.variables.q_sf_des/3, L_min=L_min)
-    print(timestamp + q_sf_des_single_and_lmin + logline)
+    print(timestamp + logline)
 
     data.current_iteration += 1
-    return lcoe #+ max([ (data.variables.dT_approach_charge_hx + data.variables.dT_approach_disch_hx - 30), 0 ])*2.
+    return lcoe 
 
 def f_callback(xk): #, data):
     # print(".... Iteration complete")
     return
 
-def optimize(thread_id, sf_interp_provider, **kwargs):
+def optimize(thread_id, GlobalHandler, **kwargs):
 
     # choose the case based on the thread_id integer
-    casename = ["north-bucket", "surround-bucket", "north-skip", "surround-skip"][thread_id % 4]
+    # casename = ["north-bucket", "surround-bucket", "north-skip", "surround-skip"][thread_id % 4]
+    casename = "surround-skip"
     case = "{:03d}_{:s}".format(thread_id, casename)
 
     # instantiate the case
-    g = G3.Gen3opt(sf_interp_provider = sf_interp_provider)
+    g = G3.Gen3opt(sf_interp_provider = GlobalHandler.sf_interp_provider)
+    g.global_handler = GlobalHandler
 
-    g.settings.is_north = 'north' in case
+    g.settings.is_north = False # 'north' in case
     
     # force attributes for optimization
     g.optimization_log = ""
@@ -152,13 +170,22 @@ def optimize(thread_id, sf_interp_provider, **kwargs):
     else:
         # Randomly choose initial guess values for most variables, but correlate some
         x0 = [random.uniform(x[0], x[1]) for x in xb]
-        x0[2] = g.variables.guess_h_tower(cycle_design_power = x0[0], solar_multiple = x0[1], is_north = g.settings.is_north) # correlate tower height guess to power
-        x0[6] = xb[6][0] + (x0[2] - xb[2][0]) * (xb[6][1] - xb[6][0]) / (xb[2][1] - xb[2][0])    # set riser_inner_diam guess at the same fraction across its range as 
-                                                                                                #  the tower height is across its range b/c they're positively correlated
-        x0[7] = xb[7][0] + (x0[2] - xb[2][0]) * (xb[7][1] - xb[7][0]) / (xb[2][1] - xb[2][0])    # set downcomer_inner_diam guess at the same fraction across its range as 
-                                                                                                #  the tower height is across its range b/c they're positively correlated
-        [receiver_height_min, receiver_height_max] = G3rec.ReceiverHeightRange(x0[5])            # get receiver height limits based on random receiver tube diameter
-        x0[4] = random.uniform(receiver_height_min, receiver_height_max)                         # choose receiver height guess within height limits
+        # correlate tower height guess to power
+        x0[2] = g.variables.guess_h_tower(cycle_design_power = x0[0], solar_multiple = x0[1], is_north = g.settings.is_north) 
+
+        # set riser_inner_diam guess at the same fraction across its range as 
+        #  the tower height is across its range b/c they're positively correlated
+        x0[6] = xb[6][0] + (x0[2] - xb[2][0]) * (xb[6][1] - xb[6][0]) / (xb[2][1] - xb[2][0])    
+
+        # set downcomer_inner_diam guess at the same fraction across its range as 
+        #  the tower height is across its range b/c they're positively correlated
+        x0[7] = xb[7][0] + (x0[2] - xb[2][0]) * (xb[7][1] - xb[7][0]) / (xb[2][1] - xb[2][0])    
+
+        # get receiver height limits based on random receiver tube diameter
+        [receiver_height_min, receiver_height_max] = G3rec.ReceiverHeightRange(x0[5])            
+        
+        # choose receiver height guess within height limits
+        x0[4] = random.uniform(receiver_height_min, receiver_height_max)                         
 
     # normalize bounds using respective guess values
     for i in range(len(x0)):
@@ -168,20 +195,13 @@ def optimize(thread_id, sf_interp_provider, **kwargs):
     g.x_scalers = [v for v in x0]   # used in the objective function for unscaling the normalized values sent by the optimizer
     x0 = [1. for v in x0]           # normalized initial guesses
 
-    g.z_best = {'z':float('inf'), 'xk':[v for v in x0], 'iter':-1}  # initialize best point tracker
+    g.z_best = {'z':float('inf'), 'xk':[v for v in x0], 'iter':-1, 'q_sf_des':float('nan'), 'l_min':float('nan')}  # initialize best point tracker
 
-    # print column labels for terminal output
-    print("time      Q_rec -> L_min  ###_field-lift      Iter: ####    LCOE     P_ref       solarm  H_tower  dni_des      H_rec     D_rec_tb  D_riser   D_dcomr  tshours   dT_chrg   dT_htHX   dT_ltHX")
-
-    # call optimize
-    # scipy.optimize.minimize(f_eval, x0, args = ((g,)), method='SLSQP', tol=0.001, 
-    #                         options={'maxiter':10, 'eps':0.1, 'ftol':0.001}, 
-    #                         bounds=xb, callback=f_callback)
-
-    scipy.optimize.fmin_slsqp(f_eval, x0, bounds=xb, args=((g,)), ieqcons=[fconst_eval], epsilon=0.1, iter=20, acc=0.001)
+    # Optimize
+    scipy.optimize.fmin_slsqp(f_eval, x0, bounds=xb, args=((g,)), ieqcons=[fconst_eval], epsilon=0.1, iter=100, acc=0.001)
 
     # report best point
-    logline = log_entry(g.z_best['xk'], g.z_best['z'], g.z_best['iter'], "***Best point:")
+    logline = GlobalHandler.log_entry(g.z_best['xk'], g.z_best['z'], g.z_best['q_sf_des'], q.z_best['l_min'], g.z_best['iter'], "***Best point:")
     g.optimization_log += "\n\n" + logline
 
     # write a summary log
@@ -192,26 +212,24 @@ def optimize(thread_id, sf_interp_provider, **kwargs):
     fout.close()
 
 
+
 if __name__ == "__main__":
 
-    north_interp_provider = G3field.load_heliostat_interpolator_provider('resource/eta_lookup_all.csv', 'north')
-    surr_interp_provider = G3field.load_heliostat_interpolator_provider('resource/eta_lookup_all.csv', 'surround')
+    GS = global_handler(G3field.load_heliostat_interpolator_provider('resource/eta_lookup_all.csv', 'surround'))        #choose 'north' or 'surround'
     
     # Run different field-lift combinations on different threads
-    # nthreads = 4
-    # nreplicates = 1
+    nthreads = 1
+    nreplicates = 1
     # -------
-    # all_args = []
-    # for i in range(nreplicates*4):
-    #     all_args.append([i, north_interp_provider if i % 2 == 0 else surr_interp_provider]) 
+    all_args = []
+    for i in range(nreplicates):
+        all_args.append([i, GS]) 
     # -------
-    # pool = multiprocessing.Pool(processes=nthreads)
-    # pool.starmap(optimize, all_args)
+    pool = multiprocessing.Pool(processes=nthreads)
+    pool.starmap(optimize, all_args)
     
-    id=3
-    sf_interp_provider = north_interp_provider if id%2 == 0 else surr_interp_provider
 
-    optimize(id, sf_interp_provider)
+    # optimize(id, GS)
 
     # x0 = [84.1, 2.5, 188.544, 789.287, 6.28, 0.375, 0.574, 0.577, 15.499, 34.613, 37.325, 37.325]
     # optimize(id, sf_interp_provider, x0=x0)

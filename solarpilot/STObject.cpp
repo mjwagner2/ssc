a7e67a39489f088f2759a8707ef60cc89c35b5fb
WHamilt2 <william.hamilton@nrel.gov>
2023-07-17 16:37:43 -0600

    updated ST object
    surface assumptions

--- solarpilot/STObject.cpp
+++ solarpilot/STObject.cpp
@@ -45,6 +45,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 using namespace std;
 
+//#define FALLPARTICLE_HERMITE_VALIDATION
 #ifdef SP_USE_SOLTRACE
 
 ST_OpticalProperties::ST_OpticalProperties()
@@ -949,7 +950,7 @@ bool ST_System::CreateSTSystem(SolarField &SF, Hvector &helios, Vect &sunvect){
 					element->Origin[1] = P->y + Floc.y;
 					element->Origin[2] = P->z + Floc.z;
 
-					element->AimPoint[0] = element->Origin[0] + Faim.i*1000.;
+					element->AimPoint[0] = element->Origin[0] + Faim.i*1000.;	// Mult. by 1000. due to numerical precision
 					element->AimPoint[1] = element->Origin[1] + Faim.j*1000.;
 					element->AimPoint[2] = element->Origin[2] + Faim.k*1000.;
 
@@ -1264,21 +1265,25 @@ bool ST_System::CreateSTSystem(SolarField &SF, Hvector &helios, Vect &sunvect){
 				std::string snout_opt_name = "SNOUT Surface";
 				copt->Name = snout_opt_name;
 				//set the optical properties. (Front)
-				copt->Front.DistributionType = 'f'; // diffuse
+				copt->Front.DistributionType = 'f'; // diffuse surface
 				copt->Front.OpticSurfNumber = 0;
 				copt->Front.ApertureStopOrGratingType = 0;
-				copt->Front.Reflectivity = 0.0; // Assuming white paint -> 0.9
-				copt->Front.Transmissivity = 0.;
+				copt->Front.Reflectivity = 0.8;
 				copt->Front.RMSSlopeError = 0.00001;
 				copt->Front.RMSSpecError = 1000.;
+
 				//back surface optics
 				copt->Back.DistributionType = 'f';
 				copt->Back.OpticSurfNumber = 0;
 				copt->Back.ApertureStopOrGratingType = 0;
-				copt->Back.Reflectivity = 0.0; // Assuming white paint  -> 0.9
-				copt->Back.Transmissivity = 0.;
+				copt->Back.Reflectivity = 0.8;
 				copt->Back.RMSSlopeError = 0.00001;
 				copt->Back.RMSSpecError = 1000.;
+
+#ifdef FALLPARTICLE_HERMITE_VALIDATION
+				copt->Front.Reflectivity = 0.0;
+				copt->Back.Reflectivity = 0.0;
+#endif // FALLPARTICLE_HERMITE_VALIDATION
 
 				//SNOUT Top Panel
 				snout_stage->ElementList.push_back(new ST_Element());
@@ -1472,22 +1477,32 @@ bool ST_System::CreateSTSystem(SolarField &SF, Hvector &helios, Vect &sunvect){
 
 			//**** Add optics stage *****//
 			copt->Name = (rv->rec_name.val).c_str();
-			//set the optical properties. This should be a diffuse surface, make it a pillbox distribution w/ equal angular reflection probability.
-			copt->Front.DistributionType = 'g'; // 'f';
+			//set the particle curtain optical properties.
+			copt->Front.DistributionType = 'g';
 			copt->Front.OpticSurfNumber = 0;
 			copt->Front.ApertureStopOrGratingType = 0;
-			copt->Front.Reflectivity = 1. - rv->absorptance.val;
-			//copt->Front.Transmissivity = 0.3; // TODO: Update this to change properties as a function of curtain height
-			copt->Front.RMSSlopeError = PI / 4.; // 0.00001;
-			copt->Front.RMSSpecError = PI / 4.; // 10000.;
+			copt->Front.Transmissivity = 0.3; // TODO: Update this to change properties as a function of curtain height
+			copt->Front.RMSSlopeError = 0.00001;
+			copt->Front.RMSSpecError = 1000.;  //PI / 4.; // 10000.;
+			copt->Front.RefractiveIndex[0] = 1.0; // Real part
 			//back
-			copt->Back.DistributionType = 'g'; // 'f';
+			copt->Back.DistributionType =  'g';
 			copt->Back.OpticSurfNumber = 0;
 			copt->Back.ApertureStopOrGratingType = 0;
+			copt->Back.Transmissivity = 0.3;
+			copt->Back.RMSSlopeError = 0.00001; // PI / 4.; // 0.00001;
+			copt->Back.RMSSpecError = 1000.; // PI / 4.; // 10000.;
+			copt->Back.RefractiveIndex[0] = 1.0;  // Real part
+
+#ifdef FALLPARTICLE_HERMITE_VALIDATION
+			copt->Front.Reflectivity = 1. - rv->absorptance.val;
+			copt->Front.RMSSlopeError = PI / 4.;
+			copt->Front.RMSSpecError = PI / 4.;
+
 			copt->Back.Reflectivity = 1. - rv->absorptance.val;
-			//copt->Front.Transmissivity = 0.3;
-			copt->Back.RMSSlopeError = PI / 4.; // 0.00001;
-			copt->Back.RMSSpecError = PI / 4.; // 10000.;
+			copt->Back.RMSSlopeError = PI / 4.;
+			copt->Back.RMSSpecError = PI / 4.;
+#endif // FALLPARTICLE_HERMITE_VALIDATION
 
 			sp_point* pos;
 			//**** Add particle curtain *****//
@@ -1530,14 +1545,16 @@ bool ST_System::CreateSTSystem(SolarField &SF, Hvector &helios, Vect &sunvect){
 					element->Su_A = 1. / surface.getSurfaceRadius();
 				}
 
-				element->InteractionType = 2; // 1; // Refraction surface for transmissivity
+				element->InteractionType = 1; // Refraction surface for transmissivity
+#ifdef FALLPARTICLE_HERMITE_VALIDATION
+				element->InteractionType = 2; // Reflection surface for reflectivity
+#endif // FALLPARTICLE_HERMITE_VALIDATION
 				element->OpticName = (rv->rec_name.val).c_str();
 				element->Optics = copt;
 				element->Comment = "Particle Curtain " + std::to_string(s);
 			}
 			
 			//**** Cavity Surfaces *****//
-			
 			// Create a diffuse-like optic surface 
 			std::string cavity_opt_name = "Cavity Surface";
 			OpticsList.push_back(new ST_OpticalPropertySet());
@@ -1547,23 +1564,30 @@ bool ST_System::CreateSTSystem(SolarField &SF, Hvector &helios, Vect &sunvect){
 			copt->Front.DistributionType = 'f';
 			copt->Front.OpticSurfNumber = 0;
 			copt->Front.ApertureStopOrGratingType = 0;
-			copt->Front.Reflectivity = rv->absorptance.val; // 0.9; // Assuming white paint -> we could assume absorptance
-			copt->Front.Transmissivity = 0.;
+			copt->Front.Reflectivity = 0.8; // TODO: we could assume absorptance
 			copt->Front.RMSSlopeError = 0.00001;
 			copt->Front.RMSSpecError = 1000.;
 			//back surface optics
 			copt->Back.DistributionType = 'f';
 			copt->Back.OpticSurfNumber = 0;
 			copt->Back.ApertureStopOrGratingType = 0;
-			copt->Front.Transmissivity = 0.; 
-			copt->Back.Reflectivity = rv->absorptance.val; // 0.9; // Assuming white paint
+			copt->Back.Reflectivity = 0.8;
 			copt->Back.RMSSlopeError = 0.00001;
 			copt->Back.RMSSpecError = 1000.;
 
-			//Bottom
+#ifdef FALLPARTICLE_HERMITE_VALIDATION
+			// TODO (BILL): The reason validation requires the cavity reflectivity to equal 1 is because the current approach uses
+			// the overlap projections of the snout and the aperture and doesn't account for the curtain...
+			// Therefore, all flux entering the aperature is assumed to hit the curtain...
+			// We could improve this by using the overlap projection of the curain, aperature, and snout...
+			// This approach would make the hermite an underestimater which is currently an overestimator
+			copt->Front.Reflectivity = 1.0;  //rv->absorptance.val; // 0.0;
+			copt->Back.Reflectivity = 1.0; //rv->absorptance.val; //0.0;
+#endif // FALLPARTICLE_HERMITE_VALIDATION
+
 			double back_cavity_offset = 0.5;   // [m] assumed distance between curtain and back of cavity 
 			// TODO (BILL): This should be normalized or set by user
-
+			//Bottom
 			r_stage->ElementList.push_back(new ST_Element());
 			element = r_stage->ElementList.back();
 			element->Enabled = true;
@@ -1756,6 +1780,8 @@ bool ST_System::CreateSTSystem(SolarField &SF, Hvector &helios, Vect &sunvect){
 			element->OpticName = cavity_opt_name;
 			element->Optics = copt;
 			element->Comment = "Cavity Front West";
+
+
 			break;
 		}
 		case Receiver::REC_GEOM_TYPE::PLANE_ELLIPSE:
